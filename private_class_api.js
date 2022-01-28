6d706e2d268f5f8a9010875fb297e0cafdbb4d86
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-01-28 20:19:01 +0100

    Create private_class_api.js

---
+++ private_class_api.js
@@ -0,0 +1,56 @@
+class Lestar{
+
+    #name;
+    #privateArray;
+
+    constructor(){
+        this.#privateArray=[];				
+    }
+
+    showAllConsole(){
+        console.log(this);
+    }
+
+    add(name,value){
+        this.#privateArray[name]=value;
+    }
+
+    set(name,value){
+        this.#privateArray[name]=value;
+    }
+
+    get(name){
+        return this.#privateArray[name];
+    }
+
+    createName(name){
+        this.#name=name;
+    }
+
+    static createObject(ilosc, czy_id=null){
+
+        let tabObjs = new Array(ilosc);
+
+        for(let i=0; i<ilosc; i++){
+            tabObjs[i]=new LestarObject();
+            if(czy_id==null || czy_id==true){
+                tabObjs[i].add('id', i);
+            }
+        }
+
+        return tabObjs;
+    }
+
+    static showAllInArrayToConsole(array){
+        for(let i=0; i<array.length; i++){
+            array[i].showAllConsole();
+        }
+    }
+
+}
+
+class LestarObject extends Lestar{
+    constructor(){
+        super();
+    }
+}
